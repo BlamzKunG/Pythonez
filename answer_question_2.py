deca8d4d38b64f7d58e62d4814601c43e054eee4
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-08-27 14:49:33 +0700

    Update answer_question_2.py

--- answer_question_2.py
+++ answer_question_2.py
@@ -1,7 +1,13 @@
 user_input = input()
 i = list(map(float, user_input.split()))
 
-for _ in range(len(i)):
-  if oh
+if i.index(max(i)) > i.index(min(i)):
+	for _ in range(len(i)):
+		if i[0] > min(i):
+			i.remove(i[0])
+else:
+	r = len(i) - (i.index(max(i)) + 1)
+	for _ in range(r):
+		i.remove(len(i) - 1)
 
-print(f"{a:.2f}")
+print(f"{r}")
