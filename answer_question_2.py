b6dca30552215c547f5b0e5577f9d721dc05ba9e
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-08-28 10:06:55 +0700

    Update answer_question_2.py

--- answer_question_2.py
+++ answer_question_2.py
@@ -1,14 +1,18 @@
 user_input = input()
 i = list(map(float, user_input.split()))
+j = list(map(float, i))
 
 if i.index(min(i)) < i.index(max(i)):
 	for _ in range(len(i)):
 		if i[0] > min(i):
 			i.remove(i[0])
 else:
-	r = len(i) - (i.index(max(i)) + 1)
-	for _ in range(r):
-		i.pop()
+    r = len(i) - (i.index(max(i)) + 1)
+    for _ in range(r):
+        i.pop()
+    if (max(i) - min(i)) < (max(j) - min(j)):
+        c = max(j) - min(j)
+        print(f"{c}")
 
 a = max(i) - min(i)
-print(f"{a}")
+print(f"{j}")
